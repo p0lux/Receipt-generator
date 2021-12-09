cf24f62d3c61955275f00062d79f9a9a0ba7c2fb
p0 <a>
2021-12-10 00:17:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- receipt_generator/dashboard/forms.py
+++ receipt_generator/dashboard/forms.py
@@ -5,3 +5,9 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.PasswordInput()
 
+
+class RegisterForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    password = forms.PasswordInput()
+    sudo = forms.BooleanField()
--- receipt_generator/dashboard/views.py
+++ receipt_generator/dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 def index(request):
@@ -9,4 +9,11 @@ def index(request):
 
 
 def login(request):
-    return render(request, 'registration/login.html')
+    form = LoginForm(request.POST)
+    if form.is_valid():
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+
+
+def register(request):
+    form = RegisterForm()
